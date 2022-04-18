8a5960256779dc6bd3e49fc2bdcd26c0cc3167cd
Ben Puryear <bpuryear@zagmail.gonzaga.edu>
2022-04-18 13:07:42 -0700

    starting branch

---
+++ datachoping.py
@@ -0,0 +1,8 @@
+# this is the file that im going to use to chop up the data
+# the datafile is over 6 million lines
+
+# i want it to be less than 100k lines
+# while also being equally distributed
+# between the classes
+
+# TODO: this later
